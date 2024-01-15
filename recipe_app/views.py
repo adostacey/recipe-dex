a04b21a83518b8cac15520eaf58af84bfafbebf5
Aaron Stacey <aaron.stacey@proton.me>
2024-01-15 12:39:29 -0600

    Add initial recipe view

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Recipe
 # Create your views here.
+class RecipeList(ListView):
+  model = Recipe
+  context_object_name = "recipe_list" 
